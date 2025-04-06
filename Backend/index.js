75f8c0965cae45448cccf9e51560ef7677f7f920
Sahasranm <sahasranmanimaran7@gmail.com>
2025-04-06 18:06:47 +0530

    ready

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,12 @@
 import express from "express";
+
+import authRoutes from  "./src/routes/auth.route.js"
+
 const app = express();
 
 app.use("/api/auth",authRoutes)
 
-app.listen(5001,()=>
+app.listen(5000,()=>
 {
     console.log("server is running");
 });
